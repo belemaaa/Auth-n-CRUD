6f43e59e1ebbcd88e122b522798ec279d6a551ae
belemaaa <graceitamunoala@gmail.com>
2023-08-21 08:10:21 +0000

    changes to delete.py

--- client/delete.py
+++ client/delete.py
@@ -3,17 +3,15 @@ import requests
 with open('token.txt', 'r') as token_file:
     token = token_file.read().strip()
 
-endpoint = 'http://127.0.0.1:8000/api/post/3/delete/'
 headers = {'Authorization': f'Bearer {token}'}
 
 pk = int(input('enter the id of the resource you wish to delete: '))
+endpoint = f'http://127.0.0.1:8000/api/post/{pk}/delete'
 try:
     pk = pk
 except pk is None:
     print(f'{pk} is not a valid id')    
 
 if pk:
-    endpoint = f'http://127.0.0.1:8000/api/post/{pk}/delete'
     response = requests.delete(endpoint, headers=headers)
-
     print(response.status_code, response.status_code==204)
